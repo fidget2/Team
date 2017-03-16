fe933661c19ddfa0472ca8fa347b23c9a4da2957
Macallister Armstrong <macallister.j.armstrong@wmich.edu>
2017-03-16 19:24:30 -0400

    fidget2 changes

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public final class Test {
+
+	public static void main(String[] args) {
+		// Macallister Armstrong's code
+		System.out.println("Hello");
+                // fidget2's code
+                System.out.println("HI");
+	}
+}
